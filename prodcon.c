1101f2a0326840fd92688b47abfabac5009d3e9c
Ramon-Diaz <a01133921@itesm.mx>
2021-10-23 14:20:06 -0700

    reading file

--- prodcon.c
+++ prodcon.c
@@ -1,5 +1,8 @@
 #include <stdio.h> // to handle printf
 #include <stdlib.h> // to handle system arguments
+#include <string.h> // to handle strcpy
+
+#include "prodcon.h"
 
 int main(int argc, char *argv[]){
     /* The main function receives the system arguments
@@ -7,20 +10,21 @@ int main(int argc, char *argv[]){
         - argc: numerical number of arguments
         - argv: the char system arguments
     */
-    int nthreads;
-    int id;
 
     // check the amount of arguments provided to the terminal
     // if the number of provided arguments is two save them to the variable
+    printf("%i\n", argc);
     if(argc == 3){
         nthreads = atoi(argv[1]);
         id = atoi(argv[2]);
+        while (fgets (filebuffer, BUFFSIZE, stdin))
+            fputs (filebuffer, stdout);
     // if no id argument is supplied go to default (0)
     } else if(argc == 2){
         nthreads = atoi(argv[1]);
         id = 0;
     // if more than 2 arguments are supplied exit
-    } else if( argc > 3 ) {
+    } else if( argc > 5 ) {
         printf("Error: Too many arguments supplied.\n");
         exit(1);
     }
---
+++ prodcon.h
@@ -0,0 +1,10 @@
+#ifndef prodcon
+#define prodcon
+
+#define BUFFSIZE 100
+
+int nthreads;
+int id;
+char filebuffer[BUFFSIZE];
+
+#endif
