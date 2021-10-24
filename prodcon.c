73b7d0cd4857729a6b7537844cb067c65052bf54
Ramon-Diaz <a01133921@itesm.mx>
2021-10-23 20:05:08 -0700

    captures all instructions and save them to args

--- prodcon.c
+++ prodcon.c
@@ -11,28 +11,41 @@ int main(int argc, char *argv[]){
         - argv: the char system arguments
     */
 
+    // allocate dynamic memory for the number of instructions
+    args = malloc(MAXINSTRUCTIONS*sizeof(char*));
     // check the amount of arguments provided to the terminal
     // if the number of provided arguments is two save them to the variable
-    printf("%i\n", argc);
-    if(argc == 3){
+    if( argc == 3 || argc == 2 ){
         nthreads = atoi(argv[1]);
-        id = atoi(argv[2]);
-        while (fgets (filebuffer, BUFFSIZE, stdin))
-            fputs (filebuffer, stdout);
-    // if no id argument is supplied go to default (0)
-    } else if(argc == 2){
-        nthreads = atoi(argv[1]);
-        id = 0;
+        // if no id argument is supplied go to default (0)
+        if( argc == 2 ){
+            id = 0;
+        } else {
+            id = atoi(argv[2]);
+        }
+        // initialize the counter index for the args array
+        int i = 0;
+        while( getline(&linebuffer, &buffsize, stdin) != -1 ){
+            // allocate space for each separate line
+            args[i] = malloc(strlen(linebuffer)+1);
+            // eliminate the \n at the end of the fgets
+            linebuffer[strcspn(linebuffer, "\n")] = 0;
+            // copy the line to the dynamic array
+            strcpy(args[i], linebuffer);
+            // increment the index by one
+            i++;
+        }
     // if more than 2 arguments are supplied exit
-    } else if( argc > 5 ) {
+    } else if( argc > 3 ) {
         printf("Error: Too many arguments supplied.\n");
         exit(1);
     }
     // if no arguments are supplied then exit
-    else {
+    else{
         printf("Error: At least one argument expected.\n");
         exit(1);
     }
-
+    // free the memory space of the dynamic memory space
+    free(args);
     return 0;
 }
--- prodcon.h
+++ prodcon.h
@@ -1,10 +1,13 @@
 #ifndef prodcon
 #define prodcon
 
-#define BUFFSIZE 100
+#define MAXINSTRUCTIONS 50 // define the maximum number of instructions from the instructions.txt file
 
-int nthreads;
-int id;
-char filebuffer[BUFFSIZE];
+int nthreads; // initialize the global variable to save the number of threads
+int id; // initialize the global variable to save the id name
+char *linebuffer; // initialize the linebuffer for the getline
+size_t buffsize = 3; // initialize the size of each line to 3 as we only have 3 characters "T2\n"
+char **args; // initialize the array, where the instructions will be saved.
+
 
 #endif
